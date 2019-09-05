8dafce7763e6d1293141ea8f81b7e5ebcefb9dec
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-09-05 09:46:52 -0700

    fixed while loop

--- spaceman.py
+++ spaceman.py
@@ -1,11 +1,19 @@
-word = "beyonce" # length is 7
+word = "bad" # length is 7
 guess = "" # length is 0
+failed = 0
 #print(len(guess))
 while len(guess) < len(word):
     guessing = input("Guess a letter: ")
     for i in word:
-        if i == guessing:
-            guess += i
-            print(guess)
-    if len(guess) >= len(word):
+        for j in guessing:
+            if i == j:
+                guess += i
+            else:
+                failed += 1
+    print(guess)
+    if failed == 4 :
+        print("No more guesses")
+        #break
+    elif len(guess) >= len(word):
         break
+# need to create a counter for FAILED ATTEMPTS
